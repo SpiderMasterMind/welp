bad0a183dd31b6b52fa5fb9f4b412b8e86dd644e
Ben Stacey <bstacey37@gmail.com>
2018-01-04 18:10:17 +0000

    create, show and index businesses ui mockups

--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -1,7 +1,7 @@
 class UiController < ApplicationController
-  # before_action do
-  #   redirect_to :root if Rails.env.production?
-  # end
+  before_action do
+    redirect_to :root if Rails.env.production?
+  end
 
   layout "application"
 
