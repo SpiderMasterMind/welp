f5eb6c06550504485ff4d7acf4dd1d1c21ef4a8d
Ben Stacey <bstacey37@gmail.com>
2018-01-09 20:53:35 +0000

    seeded DB, recent businesses template

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+12.times do
+	Business.create(title: Faker::Company.name, address: Faker::Address.street_name, city: Faker::Address.city , postcode: Faker::Address.postcode)
+end
