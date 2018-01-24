09a6413980d62059b26dfab29dc3588bbdd9b2ac
Ben Stacey <bstacey37@gmail.com>
2018-01-24 13:47:13 +0000

    new user session spec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,17 @@ class SessionsController < ApplicationController
 		redirect_to home_path if current_user
 	end
 
+  def create
+ 		user = User.find_by(email: params[:email])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect_to home_path, notice: "You are signed in!"
+		else
+			flash[:error] = "Invalid email or password"
+			redirect_to sign_in_path
+		end   
+  end
+
 
 
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -16,11 +16,48 @@ describe SessionsController do
 
 	end
 
-	describe "POST new" do
+	describe "POST new", type: :request do
+    context "with valid credentials" do
+      it "adds the signed in user id into the session cookie" do
+        user = Fabricate(:user)
+        post '/sessions', params: { email: user.email, password: user.password }
+        expect(session[:user_id]).to eq(user.id)
+      end
+
+      it "redirects to the home page" do
+        user = Fabricate(:user)
+        post '/sessions', params: { email: user.email, password: user.password }
+        expect(response).to redirect_to home_path
+      end
+
+      it "renders a sign-in success notice" do
+        user = Fabricate(:user)
+        post '/sessions', params: { email: user.email, password: user.password }
+        expect(flash[:notice]).to_not be_blank
+      end
+    end
+
+    context "with invalid credentials" do
+      it "doesn't add the credentials to the session cookie" do
+        user = Fabricate(:user)
+        post '/sessions', params: { email: user.email, password: user.password + "xyz" }
+        expect(session[:user_id]).to_not eq(user.id)
+      end
+
+      it "renders the new session template (sign in page)"
+      it "render a sign-in failure notice"
+    end
 
 	end
 
 	describe "GET destroy" do
+    context "when a user has signed in" do
+      it "removes the signed in user's credentials from the session cookie"
+      it "redirects to the home page"
+      it "renders an informative 'signed-out' notice"
+    end
+
+    context "when a user has not signed in"
 
 	end
 
